482e202cc1c1533b628de9ffca00e43f22d1b15c
Angelo Orantes <aorantesp@gmail.com>
2020-06-24 14:05:12 -0600

    Updates

---
+++ precache-manifest.b617a122cb8155dc2ce87a0dada23745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bf007f42ddb3221901ef20797a4295",
+    "url": "/countries-react-app/index.html"
+  },
+  {
+    "revision": "09cad5023469fd8b0063",
+    "url": "/countries-react-app/static/css/main.f4321004.chunk.css"
+  },
+  {
+    "revision": "1baae448d2b635b4e025",
+    "url": "/countries-react-app/static/js/2.e1aaff89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countries-react-app/static/js/2.e1aaff89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cad5023469fd8b0063",
+    "url": "/countries-react-app/static/js/main.4a06b0b6.chunk.js"
+  },
+  {
+    "revision": "d2e5a407b0da444f801f",
+    "url": "/countries-react-app/static/js/runtime-main.b3eddc95.js"
+  }
+]);
